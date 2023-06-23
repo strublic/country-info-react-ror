6f0fc534d4f33b1bdfa1650740d2b149541925c0
Gleds Strublic <gc.strublic@gmail.com>
2023-06-23 20:13:50 -0300

    Add Density and Desc fields in Countries table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_21_142346) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_23_183110) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_21_142346) do
     t.string "capital_city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "desc"
+    t.float "density"
   end
 
   create_table "users", force: :cascade do |t|
