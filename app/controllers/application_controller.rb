062d417015deebfd47a22b7467714979fca08049
Gleds Strublic <gc.strublic@gmail.com>
2023-06-25 18:49:56 -0300

    Show not found errors in Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,37 +1,43 @@
 class ApplicationController < ActionController::API
-    before_action :authorized
+  before_action :authorized
 
-    def encode_token(payload)
-      JWT.encode(payload, 'yourSecret')
-    end
-  
-    def auth_header
-      request.headers['Authorization']
-    end
-  
-    def decoded_token
-      if auth_header
-        token = auth_header.split(' ')[1]
-        begin
-          JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
-        rescue JWT::DecodeError
-          nil
-        end
+  def encode_token(payload)
+    JWT.encode(payload, 'yourSecret')
+  end
+
+  def auth_header
+    request.headers['Authorization']
+  end
+
+  def decoded_token
+    if auth_header
+      token = auth_header.split(' ')[1]
+      begin
+        JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')
+      rescue JWT::DecodeError
+        nil
       end
     end
-  
-    def logged_in_user
-      if decoded_token
-        user_id = decoded_token[0]['user_id']
-        @user = User.find_by(id: user_id)
-      end
+  end
+
+  def logged_in_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
     end
-  
-    def logged_in?
-      !!logged_in_user
-    end
-  
-    def authorized
-      render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+  def logged_in?
+    !!logged_in_user
+  end
+
+  def authorized
+    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+  end
+
+  rescue_from ActiveRecord::RecordNotFound, with: :show_not_found_errors
+
+    def show_not_found_errors(exception)
+      render json: {error: exception.message}, status: :not_found
     end
 end
