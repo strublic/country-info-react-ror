d6d70f691777ce0b286bb92e742398291e9a34d5
Gleds Strublic <gc.strublic@gmail.com>
2023-06-25 18:51:02 -0300

    Create decode_token method in spec ApiHelper

--- spec/support/api_helper.rb
+++ spec/support/api_helper.rb
@@ -1,5 +1,9 @@
 module ApiHelper
-  def generate_token()
-    jwt = JWT.encode({user_id: user.id}, 'yourSecret')
+  def generate_token(user)
+    JWT.encode({user_id: user.id}, 'yourSecret')
+  end
+
+  def decoder_token(token)
+    JWT.decode(token, 'yourSecret', true, algorithm: 'HS256')[0]
   end
 end
