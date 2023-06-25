3c35d35a1ba8e9c3e50a35032f05782b5171933a
Gleds Strublic <gc.strublic@gmail.com>
2023-06-25 18:52:25 -0300

    Add valides in models

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,10 +1,13 @@
 class Country < ApplicationRecord
-    attr_accessor :density
+  attr_accessor :density
 
-    validates :name, :capital_city, presence: true
-    # validates :population_size, :area_total, presence: true 
+  validates :name, length: 1..20, presence: true 
+  validates :capital_city, length: 1..20, presence: true 
+  validates :desc, length: 1..140, presence: true 
+  validates :population_size, length: 1..10, presence: true 
+  validates :area_total, length: 1..8, presence: true 
 
-    def density
-        begin (population_size.to_f/area_total.to_f).round(2) rescue 0 end
-    end
+  def density
+    begin (population_size.to_f/area_total.to_f).round(2) rescue 0 end
+  end
 end
