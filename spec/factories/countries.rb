d0fbba3ae795ff2068b1ce163db9d7642a3341fe
Gleds Strublic <gc.strublic@gmail.com>
2023-06-25 18:54:03 -0300

    Complete fields in Country Factorybot

--- spec/factories/countries.rb
+++ spec/factories/countries.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
   factory :country do
-    name { Faker::Address.country }
-    capital_city { Faker::Address.city }
+    name { Faker::Address.country[0..19] }
+    capital_city { Faker::Address.city[0..19] }
+    population_size { rand(1..5)**9 }
+    area_total { rand(1..5)**9 }
+    desc { Faker::Lorem.paragraph[0..139] }
   end
 end
